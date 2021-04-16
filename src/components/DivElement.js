fe94b646c6a6276cc9f2fd7024ba58812cb78867
Eddie Lee <inrez00@uw.edu>
2021-04-15 22:07:30 -0700

    init

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,16 @@
-// Import HTMLElement here
-
+// Import  here
+import HTMLElement from './HTMLElement';
 // Define class here
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super(content);
+    this.content = content;
+    this.tag = 'div';
+  }
+}
+
+const andIThinkToMyself = new DivElement('What a wonderful world');
+console.log(andIThinkToMyself.render());
 
 // Export class here
+export default DivElement;
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,17 +1,34 @@
-function Timer(seconds) {
-  this.seconds = seconds;
+// function Timer(seconds) {
+//   this.seconds = seconds;
+// }
+
+// Timer.prototype.start = function () {
+//   var instance = this;
+//   var timerInterval = setInterval(function () {
+//     if (instance.seconds === 0) {
+//       clearInterval(timerInterval);
+//     }
+
+//     console.log(instance.seconds);
+//     instance.seconds -= 1;
+//   }, 1000);
+// };
+
+class Timer {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
+
+  start() {
+    let instance = this.seconds;
+    const timerInterval = setInterval(() => {
+      if (instance === 0) {
+        clearInterval(timerInterval);
+      }
+      console.log(instance);
+      instance -= 1;
+    }, 1000);
+  }
 }
-
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
-      clearInterval(timerInterval);
-    }
-
-    console.log(instance.seconds);
-    instance.seconds -= 1;
-  }, 1000);
-};
 
 export default Timer;
